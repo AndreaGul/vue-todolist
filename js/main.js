57c9786ec295b414f6b6cad71ace72b97055540c
AndreaGul <andreagulino99@gmail.com>
2023-11-29 03:13:31 +0100

    feat:milestone 3

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@
 const { createApp } = Vue;
 // - text, una stringa che indica il testo del todo
 // - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
-// MILESTONE 2
-//   Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+// MILESTONE 3
+//   Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 createApp({
   data() {
     return {
@@ -18,11 +18,21 @@ createApp({
           done: false,
         },
       ],
+      todoChange: '',
     };
   },
   methods: {
     removeTodo(index) {
       this.todoList.splice(index, 1);
+    },
+    addTodo() {
+      const newTodo = { text: this.todoChange, done: false };
+
+      if (this.todoChange.trim().toLowerCase() !== '') {
+        this.todoList.push(newTodo);
+
+        this.todoChange = '';
+      }
     },
   },
 }).mount('#app');
